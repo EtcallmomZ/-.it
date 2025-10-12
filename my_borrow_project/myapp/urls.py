a40e99c21387283e90bd92bbd38d62972563a59e
eiweiw321 <eiweiw321@gmail.com>
2025-10-13 00:53:15 +0700

    modify login button

--- my_borrow_project/myapp/urls.py
+++ my_borrow_project/myapp/urls.py
@@ -3,6 +3,7 @@ from myapp import views
 
 urlpatterns = [
     path('',views.index),
+    path('index',views.index),
     path('login',views.login),
     path('form',views.form),
     path('user',views.user)
