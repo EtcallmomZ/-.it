cbcf5bcfbfa82ff0cb75e3bd45293356e653d87c
eiweiw321 <eiweiw321@gmail.com>
2025-10-14 03:29:43 +0700

    create a admin and redesign a signup page

--- my_borrow_project/myapp/models.py
+++ my_borrow_project/myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+# เราจะตั้งว่าเก็บฐานข้อมูลอะไรไว้บ้าง ในไฟล์นี้
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=128)
+    date = models.DateField(auto_now_add = True)
+
+    def __str__(self):
+        return f"username = {self.name} , email = {self.email}"
--- my_borrow_project/myapp/views.py
+++ my_borrow_project/myapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -8,7 +10,16 @@ def login(request):
     return render(request,"login.html")
 
 def form(request):
-    return render(request,"form.html")
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f'Account created for {username}! You can now log in')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request,"form.html" , {'form' : form})
 
 def user(request):
     return render(request,"user.html")
