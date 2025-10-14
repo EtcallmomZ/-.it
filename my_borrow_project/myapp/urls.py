0977fda28f206b8d1dd28774daf3e5a7692bb124
eiweiw321 <eiweiw321@gmail.com>
2025-10-14 19:51:11 +0700

    add a login

--- my_borrow_project/myapp/urls.py
+++ my_borrow_project/myapp/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path('',views.index),
-    path('index',views.index),
-    path('login',views.login),
-    path('form',views.form),
-    path('user',views.user)
+    path('',views.index,name = 'index'),
+    path('index',views.index,name = 'index'),
+    path('login',views.login_view, name = 'login'),
+    path('signup',views.signup, name = 'signup'),
+    path('user',views.user,name = 'user')
 ]
--- my_borrow_project/myapp/views.py
+++ my_borrow_project/myapp/views.py
@@ -1,25 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import SignUpForm # import signup มา
+#import autencation form มาอันนี้ django ให้มาเป็นฟอร์มสำเร็จ
+from django.contrib.auth.forms import AuthenticationForm 
+#import การใช้งานพร้อมเปลี่ยนชื่อไม่ให้มันซ้ำกัน
+from django.contrib.auth import authenticate , login as auth_login , logout as auth_logout 
 
 # Create your views here.
 def index(request):
     return render(request,"index.html")
-def login(request):
-    return render(request,"login.html")
-
-def form(request):
+def login_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = AuthenticationForm(request,data = request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(username=username,password=password)
+            if user is not None:
+                auth_login(request,user)
+                messages.success(request, f"Welcome back ,{username}")
+                return redirect('user')
+            else:
+                messages.error(request,"Invalid username or password.")
+        else:
+            messages.error(request,"Invalid username or password.")
+    else:
+        form = AuthenticationForm()
+    return render(request,"login.html", {'form': form})
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
             messages.success(request, f'Account created for {username}! You can now log in')
             return redirect('login')
+        else:
+            messages.error(request, 'Please correct the errors below.')
     else:
-        form = UserCreationForm()
-    return render(request,"form.html" , {'form' : form})
+        form = SignUpForm()
+    return render(request, "signup.html", {'form': form})
 
 def user(request):
     return render(request,"user.html")
