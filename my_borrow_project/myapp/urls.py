2de08fbe036baeb0cecf15bb270c9a2103a9e347
eiweiw321 <eiweiw321@gmail.com>
2025-10-15 02:12:16 +0700

    สร้่างระบบยืมของให้ผู้ใช้งาน ฟอร์มยกเลิก สถานะการยืม

--- my_borrow_project/myapp/urls.py
+++ my_borrow_project/myapp/urls.py
@@ -2,9 +2,15 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path('',views.index,name = 'index'),
+    path('', views.index, name='index'), 
     path('index',views.index,name = 'index'),
-    path('login',views.login_view, name = 'login'),
-    path('signup',views.signup, name = 'signup'),
-    path('user',views.user,name = 'user')
+    path('login', views.login_view, name='login'),
+    path('signup/', views.signup, name='signup'),
+
+    path('borrow/<int:item_id>/form/', views.created_borrow_request, name='create_borrow_request'),
+    path('user/status/', views.user_status, name='user_status'),
+    path('request/cancel/<int:request_id>/', views.cancle_request, name='cancel_request'),
+
+    path('user/', views.user, name='user')
+
 ]
