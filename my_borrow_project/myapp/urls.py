211025b9079f38daf78129a565ddc8c847a632fb
eiweiw321 <eiweiw321@gmail.com>
2025-10-16 03:20:18 +0700

    ระบบช้งานได้

--- my_borrow_project/myapp/urls.py
+++ my_borrow_project/myapp/urls.py
@@ -1,16 +1,21 @@
 from django.urls import path
 from myapp import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', views.index, name='index'), 
     path('index',views.index,name = 'index'),
     path('login', views.login_view, name='login'),
     path('signup/', views.signup, name='signup'),
-
-    path('borrow/<int:item_id>/form/', views.created_borrow_request, name='create_borrow_request'),
+    
+    path('logout/',  auth_views.LogoutView.as_view(next_page='index'), name='logout'),
+    path('borrow/<int:item_id>/form/', views.borrow_form, name='create_borrow_request'),
     path('user/status/', views.user_status, name='user_status'),
     path('request/cancel/<int:request_id>/', views.cancle_request, name='cancel_request'),
-
-    path('user/', views.user, name='user')
+    path('user/', views.user, name='user'),
+    # จัดการหมวดหมู่และสินค้า
+    path('category/<int:category_id>/',views.item_by_category,name='item_by_category'),
+    path('item/<int:item_id>/', views.item_detail ,name='item_detail')
 
 ]
